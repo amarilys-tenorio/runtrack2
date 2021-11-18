29f63d4a16a4ecf4251605ce3e29253bc59fa5e1
Amarilys Tenorio <amarilys.tenerio@laplateforme.io>
2021-11-18 19:10:05 +0100

    job01 fait

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,11 +1,15 @@
 <?php
-$i = 0; 
-while ($i <=1337 ) 
-{
-if ($i==42) 
-echo"<b><u>",$i,"</b></u><br>"; 
-else{ echo $i,"<br>"; 
-} 
-$i++; 
+
+$counter = -1;
+
+while ($counter <= 1337){
+  $counter++;
+
+if ($counter == 42) {
+  echo "<b><u>$counter</b></u>";
+}
+elseif($counter != 42){
+  echo $counter, '<br>';
+}
 }
 ?>
