d982826ed2b9da97ff76fd340ac24d03ffad3d1e
Amarilys Tenorio <amarilys.tenerio@laplateforme.io>
2021-11-22 11:36:20 +0100

    job02 fait

---
+++ jour09/job02/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $bdd = mysqli_connect("localhost","root","","jour08");
+    $cont=mysqli_query($bdd,"SELECT * FROM `salles` ");
+    $contenu =mysqli_fetch_all($cont,MYSQLI_ASSOC);    
+    ?>
+     <table border=1>
+       <thead>
+
+    <?php
+
+        echo "<tr>";
+        foreach ($contenu[0] as $row =>$ligne){
+        echo "<th>",$row,"</th>";
+        }
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        foreach ($contenu as $conte => $value){
+        echo "<tr>";
+        foreach($value as $conte2 => $value2){
+        echo "<td>",$value2,"</td>";
+
+        }
+        echo"</tr>";
+        }
+        echo"</tbody>";
+        echo "</table>";
+
+    ?>
+</body>
+</html>
